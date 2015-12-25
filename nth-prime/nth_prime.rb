7ac980245f6f05bf7836827a06e293ebe63b11f7
Chad Ellison <chad.ellison0123@gmail.com>
2015-12-25 12:45:25 -0500

    primes

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -3,23 +3,16 @@ require 'prime'
 class Prime
   def self.nth(number)
     raise(ArgumentError) if number < 1
-    if number == 1
-      2
-    elsif number == 2
-      3
-    else
-      primes = (1..500_000).to_a.select { |n| n.odd? }
-      primes.reject! { |n| n % 3 == 0 }
-      primes.unshift(3)
-      primes.unshift(2)
-      n = 3
-      until n == 10001
-        primes.reject! do |num|
-          num % n == 0 unless num == n
-        end
-        n += 1
+    primes = (2..500_000).to_a.select { |n| n.odd? }
+    primes.reject! { |n| n % 3 == 0 }
+    primes = [1, 2, 3] + primes
+    n = 3
+    until n == 10001
+      primes.reject! do |num|
+        num % n == 0 unless num == n
       end
-      primes[number]
+      n += 1
     end
+    primes[number]
   end
 end
