324685fd5dad522ab0a43c348267ecf4b6d20d40
Chad Ellison <chad.ellison0123@gmail.com>
2015-12-28 09:26:56 -0500

    leap

--- leap/leap.rb
+++ leap/leap.rb
@@ -1,13 +1,11 @@
 class Year
   VERSION = 1
   def self.leap?(year)
-    if year % 4 == 0
-      if year % 400 == 0 || year % 100 != 0
-        "Yes, #{year} is a leap year"
-        true
-      end
+    if year % 400 == 0
+      true
+    elsif year % 4 == 0 && year % 100 != 0
+      true
     else
-      "No, #{year} is not a leap year"
       false
     end
   end
