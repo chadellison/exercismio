8d05dfdc0308b6dba0202c632dea59ca552a125d
Chad Ellison <chad.ellison0123@gmail.com>
2015-12-28 09:25:49 -0500

    hamming

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -2,14 +2,11 @@ class Hamming
   VERSION = 1
   def self.compute(strand_1, strand_2)
     if strand_1.length != strand_2.length
-      raise(ArgumentError)
+      raise ArgumentError, "Each strand must be the same length"
     end
     dna_1 = strand_1.chars
     dna_2 = strand_2.chars
     compare = dna_1.zip(dna_2)
-    counter = compare.count do |strand|
-      strand[0] != strand[1]
-    end
-    counter
+    compare.count { |first, last| first != last }
   end
 end
