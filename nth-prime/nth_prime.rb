d41bc2566d1cbf96f3c0caf99000fd870bfb5469
Chad Ellison <chad.ellison0123@gmail.com>
2016-01-01 23:25:57 -0700

    nth_prime

--- nth-prime/nth_prime.rb
+++ nth-prime/nth_prime.rb
@@ -1,12 +1,22 @@
 class Prime
-  def self.nth(number)
-    raise ArgumentError, "Number must be greater than zero" if number < 1
-    primes = []
-    n = 2
-    until n > number * number.to_s.length * 2.7
-      primes << n unless (2...n).to_a.detect { |num| n % num == 0 }
-      n += 1
+  def self.nth(n)
+    raise ArgumentError, "Number must be greater than zero" if n < 1
+    count = 0
+    next_num = 1
+    nth_prime = 2
+    
+    until count == n
+      next_num += 1
+      if prime(next_num)
+        count += 1
+        nth_prime = next_num
+      end
     end
-    primes[number -1]
+    nth_prime
+  end
+
+  def self.prime(num)
+    factors = (2..num / 2).to_a
+    factors.any? { |factor| num % factor == 0} ? false : true
   end
 end
